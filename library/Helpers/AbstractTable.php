d5f7b5c281dbd5155833ed8322b859c146c4519a
lefterisk <lefteris.kokkonas@gmail.com>
2015-02-05 17:49:30 +0000

    Initial commit

---
+++ library/Helpers/AbstractTable.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Helpers;
+
+use Zend\Db\TableGateway\TableGateway;
+
+
+abstract class AbstractTable
+{
+    protected $_tableGateway;
+
+    public function __construct(TableGateway $tableGateway)
+    {
+        $this->_tableGateway = $tableGateway;
+    }
+
+    public function fetchAll()
+    {
+        $resultSet = $this->_tableGateway->select();
+        return $resultSet;
+    }
+
+    public function get($id)
+    {
+        $id  = (int) $id;
+        $rowset = $this->_tableGateway->select(array('id' => $id));
+        $row = $rowset->current();
+        if (!$row) {
+            throw new \Exception("Could not find row $id");
+        }
+        return $row;
+    }
+
+    public function fetch($where = array())
+    {
+        // If a number is given as the $where argument then assume we're trying
+        // to fetch based on a single primary key and try to grab that record.
+        if (is_numeric($where)) {
+            $where = array('id' => $where);
+        }
+
+        if (is_object($where)) {
+            $results = $this->_tableGateway->selectWith($where);
+        } else {
+            $results = $this->_tableGateway->select($where);
+        }
+
+        if ($results->count() === 0) {
+            return false;
+        }
+
+        return $results;
+    }
+
+    abstract public function save($object);
+
+    public function delete($id)
+    {
+        $this->_tableGateway->delete(array('id' => (int) $id));
+    }
+}
